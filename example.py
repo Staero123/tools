f495e225d218cd17cdfd6931db1381f449ee6a06
Staero123 <153693037+Staero123@users.noreply.github.com>
2023-12-13 09:35:06 +0800

    Update example.py

--- example.py
+++ example.py
@@ -7,11 +7,11 @@ st.markdown(f"Tools ID is : {Tools_ID_input} ")
 Employee_ID_input = st.text_area('Employee Id')
 st.markdown(f"Employee ID is : {Employee_ID_input} ")
 
-dialect = "mysql"
+dialect = "pymysql"
 host = "localhost"
 port = 3307
 database = "asset managament"
 username = "root"
 password = "70013029"
 
-conn = st.connection('mysql', type='sql')
+conn = st.connection('pymysql', type='sql')
