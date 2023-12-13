e0b3eb2c646264dc7a86014f3dc9cfa801f58b6a
Staero123 <153693037+Staero123@users.noreply.github.com>
2023-12-13 13:29:56 +0800

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,12 @@ import streamlit as st
 import pandas as pd
 import mysql.connector
 
+st.title('Tools Log')
+Tools_ID_input = st.text_area('Tools Id')
+st.markdown(f"Tools ID is : {Tools_ID_input} ")
+Employee_ID_input = st.text_area('Employee Id')
+st.markdown(f"Employee ID is : {Employee_ID_input} ")
+
 def app():
     conn = mysql.connector.connect( host="localhost",
                                     port="3307",
@@ -10,14 +16,10 @@ def app():
                                     db="asset management"
                                   )
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO main (EmployeeID,ToolsID,Check_In_Time,LogID) VALUES (%s,%s,%s,%s)',(2098789,123456,now.strftime('%Y-%m-%d %H:%M:%S'),2))
+    cursor.execute('INSERT INTO main (EmployeeID,ToolsID,Check_In_Time,LogID) VALUES (%s,%s,%s,%s)',(Employee_ID_input,Tools_ID_input,now.strftime('%Y-%m-%d %H:%M:%S'),2))
     conn.commit()
 
 
     conn.close()
 
-st.title('Tools Log')
-Tools_ID_input = st.text_area('Tools Id')
-st.markdown(f"Tools ID is : {Tools_ID_input} ")
-Employee_ID_input = st.text_area('Employee Id')
-st.markdown(f"Employee ID is : {Employee_ID_input} ")
+
