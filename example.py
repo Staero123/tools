c6d38d90e930ac3061b5a61bee82a56a64bbc5b6
Staero123 <153693037+Staero123@users.noreply.github.com>
2023-12-13 09:36:31 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pymysql
+
 st.title('Tools Log')
 
 Tools_ID_input = st.text_area('Tools Id')
@@ -7,11 +7,11 @@ st.markdown(f"Tools ID is : {Tools_ID_input} ")
 Employee_ID_input = st.text_area('Employee Id')
 st.markdown(f"Employee ID is : {Employee_ID_input} ")
 
-dialect = "pymysql"
+dialect = "mysql"
 host = "localhost"
 port = 3307
 database = "asset managament"
 username = "root"
 password = "70013029"
 
-conn = st.connection('pymysql', type='sql')
+conn = st.connection('sql')
