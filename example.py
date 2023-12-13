4f41f456ba8a47380dfa43e2cb9ae8bef5e974f9
Staero123 <153693037+Staero123@users.noreply.github.com>
2023-12-13 13:31:06 +0800

    Update example.py

--- example.py
+++ example.py
@@ -8,18 +8,11 @@ st.markdown(f"Tools ID is : {Tools_ID_input} ")
 Employee_ID_input = st.text_area('Employee Id')
 st.markdown(f"Employee ID is : {Employee_ID_input} ")
 
-def app():
-    conn = mysql.connector.connect( host="localhost",
-                                    port="3307",
-                                    user="root",
-                                    passwd="70013029",
-                                    db="asset management"
-                                  )
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO main (EmployeeID,ToolsID,Check_In_Time,LogID) VALUES (%s,%s,%s,%s)',(Employee_ID_input,Tools_ID_input,now.strftime('%Y-%m-%d %H:%M:%S'),2))
-    conn.commit()
 
-
-    conn.close()
+conn = mysql.connector.connect(host="localhost",port="3307",user="root",passwd="70013029",db="asset management")
+cursor = conn.cursor()
+cursor.execute('INSERT INTO main (EmployeeID,ToolsID,Check_In_Time,LogID) VALUES (%s,%s,%s,%s)',(Employee_ID_input,Tools_ID_input,now.strftime('%Y-%m-%d %H:%M:%S'),2))
+conn.commit()
+conn.close()
 
 
