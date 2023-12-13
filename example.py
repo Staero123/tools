f669897b76fad23aa41f5ae665b8d5f33e73c13c
Staero123 <153693037+Staero123@users.noreply.github.com>
2023-12-13 10:03:53 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,10 +1,7 @@
 import streamlit as st
 conn = st.connection('mysql', type='sql')
 
-
-
 st.title('Tools Log')
-
 Tools_ID_input = st.text_area('Tools Id')
 st.markdown(f"Tools ID is : {Tools_ID_input} ")
 Employee_ID_input = st.text_area('Employee Id')
