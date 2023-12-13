bfead8047123538b6cd7598d0d466894b913c41f
Staero123 <153693037+Staero123@users.noreply.github.com>
2023-12-13 09:14:30 +0800

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,6 @@ import streamlit as st
  
 st.title('Tools Log')
 
-Tools_ID_input = st.number_area('Tools Id','Employee ID')
+Tools_ID_input = st.text_area('Tools Id','Employee ID')
 st.markdown(f"Tools ID is : {Tools_ID_input} ")
 
